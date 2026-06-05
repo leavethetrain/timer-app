ca29468919e2576f643f06b6607ff2c64c078b98
leavethetrain <musik201092@web.de>
2026-06-05 10:21:34 +0200

    start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-function userInput() {
+function useInput() {
   const [input, setInput] = useState(0);
 
   function inputChange(event: React.ChangeEvent<HTMLInputElement>) {
@@ -12,8 +12,35 @@ function userInput() {
   return { input, inputChange };
 }
 
+function useTimer(startTime: number) {
+  const [leftTime, setLeftTime] = useState(startTime);
+  const countRef = useRef(null as any);
+
+  useEffect(() => {
+    setLeftTime(startTime);
+  }, [startTime]);
+
+  function startButton() {
+    if (countRef.current !== null) return;
+
+    countRef.current = setInterval(() => {
+      setLeftTime((time) => {
+        if (time <= 0) {
+          clearInterval(countRef.current);
+          countRef.current = null;
+          return 0;
+        }
+        return time - 0.01;
+      });
+    }, 10);
+  }
+
+  return { leftTime, startButton, setLeftTime };
+}
+
 function App() {
-  const { input, inputChange } = userInput();
+  const { input, inputChange } = useInput();
+  const { leftTime, startButton, setLeftTime } = useTimer(input);
   return (
     <div className="timer text">
       <h1 className="title text">Timer</h1>
@@ -25,10 +52,10 @@ function App() {
         onChange={inputChange}
       ></input>
       <p>time left:</p>
-      <p>{input.toFixed(3)}s</p>
+      <p>{leftTime.toFixed(3)}s</p>
 
       <div className="button text">
-        <button>start</button>
+        <button onClick={startButton}>start</button>
         <button>pause</button>
         <button>reset</button>
       </div>
